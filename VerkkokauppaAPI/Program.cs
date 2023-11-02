b94a33ef6ea1eb8e77de785cd875a77a83289f75
osifa <sofiashsavolainen@gmail.com>
2023-11-02 15:32:27 +0200

    purchase maps
    Started to make Get and post to purchases.

--- VerkkokauppaAPI/Program.cs
+++ VerkkokauppaAPI/Program.cs
@@ -18,4 +18,22 @@ app.MapGet("/getproductid/{name}", (string name) => database.GetProductId(name))
 
 app.MapPost("/addproduct", (Tuote tuote) => database.AddProduct(tuote.name, tuote.productCtgory, tuote.productCtgory2, tuote.price, tuote.amount, tuote.img, tuote.description));
 
+// Sofian tilaukset - Purchases -mapit
+//AddPurchase
+app.MapPost("/addpurchase", (Tilaukset tilaus) => database.AddPurchase(tilaus.asiakas_id, tilaus.tilauspaiva, tilaus.toimitusosoite, tilaus.tilauksen_hinta, tilaus.tilauksen_tila, tilaus.lisatiedot));
+
+// FindPurchaseId
+app.MapGet("/findpurchaseid/{id}", (int id) => database.FindPurchaseId(id));
+
+// FindPurchaseCustomerId
+app.MapGet("/findpurchasecustomerid/{id}", (int customer_id) => database.FindPurchaseCustomerId(customer_id));
+
+// FindPurchase_bydate
+app.MapGet("/findpurchasebydate/{date}", (string date) => database.FindPurchase_bydate(date));
+
+// DeletePurchaseById
+
+// PrintAllPurchases
+app.MapGet("/printallpurchases", () => database.PrintAllPurchases());
+
 app.Run();
