040b794f832ab2d38f5360401186a4cc3c4e39c0
ToniLehtonen <11toni.lehtonen@gmail.com>
2023-11-02 11:21:50 +0200

    ProductAPI beginnings

--- VerkkokauppaAPI/Program.cs
+++ VerkkokauppaAPI/Program.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 using Microsoft.Data.Sqlite;
+// JOS HERJAA NIIN RUNNAA: dotnet add package Microsoft.Data.Sqlite
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -8,14 +9,15 @@ var database = new Databaselogics();
 
 app.MapGet("/", () => "Tervetuloa verkkokauppaan!");
 
-// JOS HERJAA NIIN RUNNAA: dotnet add package Microsoft.Data.Sqlite
-
 // Nimeä polku SAMALLA NIMELLÄ KUIN METODI jota kutsutaan
 // Tee POSTEIHIN recordi Databaselogicsiin
 // Muuta Databaselogicsiin metodeihin connection niin, että ei ota sitä parametrina, vaan metodin sisällä luo ja open ja close
 
-app.MapGet("/getproductid/{name}", (string name) => database.GetProductId(name));
+#region ProductMapping
+    app.MapPost("/addproduct", (Product product) => database.AddProduct(product.name, product.productCtgory, product.productCtgory2, product.price, product.amount, product.img, product.description));
+    app.MapGet("/getproductid/{name}", (string name) => database.GetProductId(name));
+    app.MapGet("/getallproductnames", () => database.GetAllProductNames());
+#endregion
 
-app.MapPost("/addproduct", (Tuote tuote) => database.AddProduct(tuote.name, tuote.productCtgory, tuote.productCtgory2, tuote.price, tuote.amount, tuote.img, tuote.description));
 
 app.Run();
